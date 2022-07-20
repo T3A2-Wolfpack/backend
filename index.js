e44dfe9212f27d28e0e4042c786eda7f32c4cf67
Chris Back <chrisback011996@gmail.com>
2022-07-20 20:25:48 +1000

    Refactored for testing

--- index.js
+++ index.js
@@ -1,42 +1,7 @@
-require("dotenv").config();
-
-const express = require("express");
-const mongoose = require("mongoose");
-const whiskeyRoutes = require("./routes/whiskeys");
-const tastingRoutes = require("./routes/tastings");
-const userRoutes = require("./routes/users");
-
-const app = express();
+const app = require('./app')
 
 const port = process.env.PORT || 4000;
-
-const cors = require("cors")
-
-app.use(express.json());
-
-app.use(cors())
-
-// global middleware to log requests
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
+// listen to port
+app.listen(port, () => {
+  console.log("listening for requests on port", port);
 });
-
-// routes
-app.use("/api/whiskeys", whiskeyRoutes);
-app.use("/api/whiskeys/:id/tastings", tastingRoutes);
-app.use("/api/users", userRoutes);
-
-// database connection
-mongoose
-  .connect(process.env.ATLAS_DB_URL)
-  .then(() => {
-    console.log("connected to database");
-    // listen to port
-    app.listen(port, () => {
-      console.log("listening for requests on port", port);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  }); 
